6dd3d9151fdb7acf0a8ccb2d4f593b714cfbadd7
arpanghosh2416 <arpanghosh2416@gmail.com>
2022-11-18 00:02:07 +0530

    Security added

---
+++ src/main/java/com/company/demo/jwt/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.company.demo.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+	private User user;
+	private String jwt;
+
+}
